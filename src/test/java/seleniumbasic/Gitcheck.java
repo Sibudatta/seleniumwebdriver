ce433f7900b37ea6e543b1919c1841116398454a
Sibudatta Dhal <sibudatta1997@gmail.com>
2025-02-19 21:34:09 +0530

    update feature commit for same branch

--- src/test/java/seleniumbasic/Gitcheck.java
+++ src/test/java/seleniumbasic/Gitcheck.java
@@ -3,6 +3,7 @@ package seleniumbasic;
 public class Gitcheck {
    public static void main(String[] args) {
 	System.out.println("new branch added");
+	System.out.println("new branch code second commit");
 }
 }
 
